203209c50ec2a094b5573243aeb23a833dd3a975
VitorMScolari <vmarcals@hotmail.com>
2021-08-11 12:35:36 -0300

    finished button functions

--- script.js
+++ script.js
@@ -5,73 +5,97 @@ const eraser = document.querySelector('#erase');
 const rgbColor = document.querySelector('#rgbColor');
 const sliderText = document.querySelector('.sliderText');
 const sliderDiv = document.querySelector('.sliderDiv');
-
+const slider = document.querySelector('.slider');
 
 let toggleRGB = false;
 let toggleEraser = false;
 let painting = false;
+const gridNumber = 10;
+let currentSize = gridNumber
 
-let count = 0;
+grid.onmousemove = (e) => changeSliderText(e.target.value);
+grid.onchange = (e) => changeSize(e.target.value);
 
-
-
-function changeSliderText() {
-    sliderDiv.innerHTML = `${input[value]} x ${value}`;
+function setCurrentSize(newSize) {
+    currentSize = newSize
 }
 
-for(let i=0; i < 20; i++) {
-    container.style.gridTemplateColumns = `repeat(${i}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${i}, 1fr)`
-    count++;
+function changeSize(value) {
+    setCurrentSize(value);
+    changeSliderText(value)
+    reloadGrid()
 }
 
-count--;
+function reloadGrid() {
+    container.innerHTML = '';
+    gridSize(currentSize)
+}
 
-const countTotal = (count*count);
+function changeSliderText(value) {
+    sliderText.innerHTML = `Grid size: ${value} x ${value}`;
+}
 
 
-for(let i=0; i < countTotal; i++) {
+// let count = 0;
+
+function gridSize(value) {
+
+// for(let i=0; i < value; i++) {
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${value}, 1fr)`
+    // count++;
+// }
+
+// count--;
+
+// const countTotal = (count*count);
+
+for(let i=0; i < (value * value); i++) {
     const divs = document.createElement('div');
     divs.classList.add('main');
+    divs.addEventListener('mousemove', mainDraw)
+    divs.addEventListener('mousedown', startDrawing)
+    divs.addEventListener('mouseup', stopDrawing)
     container.appendChild(divs);
+}
 
 }
 
-
-
-const main = Array.from(document.querySelectorAll('.main'));
+// const main = Array.from(document.querySelectorAll('.main'));
 
 
 function mainDraw() {
     if (!painting) return;
 
-    this.style.backgroundColor = 'white';
-
-    if(toggleRGB == true) {
+    if (toggleEraser == false && toggleRGB == false) {
+        this.style.backgroundColor = 'white';
+    } else if (toggleRGB == true) {
         this.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
         hue++;
         if(hue >= 360) {
             hue = 0;
-        } else if (toggleEraser == true) {
+         } else if (toggleEraser == true) {
             this.style.backgroundColor = 'black';
         }
     } 
+    
     
 
 }
 
 function startDrawing() {
     painting = true;
-    this.style.backgroundColor = 'white';
-
-    if(toggleRGB == true) {
+    
+    if (toggleEraser == false && toggleRGB == false) {
+        this.style.backgroundColor = 'white';
+    } else if(toggleRGB == true) {
         this.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
         hue++;
         if(hue >= 360) {
             hue = 0;
-        } else if (toggleEraser == true) {
+         } else if (toggleEraser == true) {
             this.style.backgroundColor = 'black';
-        }
+         }
     }
 
 }
@@ -82,9 +106,9 @@ function stopDrawing() {
 }
 
 
-main.forEach(main => main.addEventListener('mousemove', mainDraw))
-main.forEach(main => main.addEventListener('mousedown', startDrawing))
-main.forEach(main => main.addEventListener('mouseup', stopDrawing))
+// main.forEach(main => main.addEventListener('mousemove', mainDraw))
+// main.forEach(main => main.addEventListener('mousedown', startDrawing))
+// main.forEach(main => main.addEventListener('mouseup', stopDrawing))
 container.addEventListener('mouseleave', () => painting = false)
 
 
@@ -92,32 +116,32 @@ let hue = Math.floor(Math.random() * 10) + 1;
 
 function toggleColor() {
     toggleEraser = false;
-    
+
     if (toggleRGB == false) {
         toggleRGB = true;
     } else {
         toggleRGB = false;
     }
-
 }
 
 
 rgbColor.addEventListener('click', toggleColor);
 
 
-function cleanGrid() {
-    main.forEach(main => main.style.backgroundColor = null);
-}
-
-clear.addEventListener('click', cleanGrid);
+clear.addEventListener('click', reloadGrid);
 
 
-function eraseGrid() {
+function eraseGrid() {    
     if (toggleEraser == false) {
         toggleEraser = true;
-    } else {
+    } else if (toggleEraser == true) {
         toggleEraser = false;
     }
 }
 
 eraser.addEventListener('click', eraseGrid);
+
+
+window.onload = () => {
+    gridSize(gridNumber)
+}
